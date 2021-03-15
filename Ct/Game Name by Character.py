e1d9805f04b5be7769f9657052aba2bed05a229b
gokul2507 <66557183+gokul2507@users.noreply.github.com>
2021-03-15 15:05:28 +0530

    Create Game Name by Character.py

---
+++ Ct/Game Name by Character.py
@@ -0,0 +1,19 @@
+a=input().strip()
+s="Invalid"
+if a=='a':
+    s="Athletics"
+elif a=='b':
+    s="Basketball"
+elif a=='c':
+    s="Cricket"
+elif a=='d':
+    s="Discus throw"
+elif a=='g':
+    s="Golf"
+elif a=='h':
+    s="Hockey"
+elif a=='f':
+    s="Football"
+elif a=='t':
+    s="Tennis"
+print(s)
