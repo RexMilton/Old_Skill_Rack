76406f527f0a7ee9e0b668ddb7639c26b6949180
gokul2507 <66557183+gokul2507@users.noreply.github.com>
2021-03-16 19:39:06 +0530

    Update solution.java

--- Ct/void alternateSort(int arr[], int LENGTH)/solution.java
+++ Ct/void alternateSort(int arr[], int LENGTH)/solution.java
@@ -18,24 +18,24 @@ public class Hello {
             System.out.print(arr[index]+" ");
         }
     }
-private static void alternateSort(int arr[], int LENGTH) {
-    for(int i=0;i<LENGTH;i++){
-        int t=i;
-        for(int j=i+1;j<LENGTH;j++){
-            if(i%2!=0){
-                if(arr[t]>arr[j])
-                t=j;
+    private static void alternateSort(int arr[], int LENGTH) {
+        for(int i=0;i<LENGTH;i++){
+            int t=i;
+            for(int j=i+1;j<LENGTH;j++){
+                if(i%2!=0){
+                    if(arr[t]>arr[j])
+                    t=j;
+                }
+                else{
+                    if(arr[t]<arr[j])
+                    t=j;
+                }
             }
-            else{
-                if(arr[t]<arr[j])
-                t=j;
+            if(i!=t){
+                arr[t]+=arr[i];
+                arr[i]=arr[t]-arr[i];
+                arr[t]=arr[t]-arr[i];
             }
-        }
-        if(i!=t){
-            arr[t]+=arr[i];
-            arr[i]=arr[t]-arr[i];
-            arr[t]=arr[t]-arr[i];
         }
     }
-}
 } // end of the class
