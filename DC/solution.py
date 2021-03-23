a4c72cf22c6a1a15315cfe3e5e3b3bbbc8e9b390
RexMilton <80641124+RexMilton@users.noreply.github.com>
2021-03-23 18:20:05 +0530

    Update solution.py

--- DC/solution.py
+++ DC/solution.py
@@ -1,9 +1,9 @@
-a=int(input())
-z=[input().strip() for _ in range(a)]
-k=int(input())
-l=len(z[0])
-for i in range(a):
-    print(end=z[i][0])
-    for j in range(1,l-1):
-        print(end=z[(i+k)%a][j])
-    print(z[i][-1])
+n=int(input())
+l=list(map(int,input().split()))
+r,f=1,1
+for i in l:
+    if i-r!=0:
+        print(r,i-1,sep='-',end=' ')
+        f=0 
+    r=i+1 
+if f:print(-1)
