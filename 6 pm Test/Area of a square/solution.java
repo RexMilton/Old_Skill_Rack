6e468a2dbc94a407ff5efa1cf28be57413767f3e
RexMilton <80641124+RexMilton@users.noreply.github.com>
2021-03-24 19:31:28 +0530

    Create solution.java

---
+++ 6 pm Test/Area of a square/solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.print(n/4*n/4);
+
+	}
+}
