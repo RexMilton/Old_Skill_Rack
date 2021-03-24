820163626205984c9271f8edfebf2ee735659554
gokul2507 <66557183+gokul2507@users.noreply.github.com>
2021-03-24 22:12:08 +0530

    Update solution.java

--- DC/solution.java
+++ DC/solution.java
@@ -1,3 +1,11 @@
+import java.util.*;
+
+interface Pair {
+
+    public long getHCF(int a, int b);
+
+    public boolean areCoPrime(int a, int b);
+}
 class Node implements Pair{
         public long getHCF(int a, int b){
             if(a==0) return b;
@@ -8,3 +16,14 @@ class Node implements Pair{
             return false;
         }
     }
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        Pair pair = new Node();
+        System.out.println("HCF: " + pair.getHCF(a, b));
+        System.out.println(pair.areCoPrime(a, b) ? "YES" : "NO");
+    }
+}
