97f25ae1cd7e0d1c9dd93ef4c72bb9dab3ec69f4
gokul2507 <66557183+gokul2507@users.noreply.github.com>
2021-03-27 20:11:02 +0530

    Update solution.py

--- 6 pm Test/Turning Book Pages/solution.py
+++ 6 pm Test/Turning Book Pages/solution.py
@@ -1,9 +1,2 @@
-n,p=int(input()),int(input())
-f,b=p//2,((n-p)//2)+1
-if n%2==1:
-    if n-p==1:
-        b=0
-    else:
-        b=((n-p)//2)
-l=[f,b]
-print(min(l))
+a,b=int(input()),int(input())
+print(min(b//2,(a-b)//2+(a+1)%2))
